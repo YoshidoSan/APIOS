42d9d771a0f6b83831550db3cdaa2ea560907d90
Kacper Marchlewicz <115720673+YoshidoSan@users.noreply.github.com>
2023-05-15 09:29:58 +0200

    Add files via upload

---
+++ na ostatnie laby/reszta/reszta/reszta/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  animacja_lab4
+//
+//  Created by apios on 14/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var flag = true
+    @State private var Scale: CGFloat = 1.0
+    @State private var position: CGSize = .zero
+    @State private var position2: CGSize = .zero
+    var destination = CGPoint(x: 500, y: 500)
+    var body: some View {
+        VStack {
+            Circle()
+                .foregroundColor(.black)
+                .frame(width: 120, height: 120)
+                .offset(x: 0, y: 60)
+        }
+        VStack {
+            Circle()
+                .foregroundColor(.red)
+                .frame(width: 120, height: 120)
+            
+                .scaleEffect(Scale)
+                .offset(x: position.width, y: position.height)
+                .onAppear(){
+                    self.position = CGSize(width: 0, height: -800)//zaczynamy od góry ekranu
+                    withAnimation(.linear(duration: 2).repeatForever()) {
+                        self.position = CGSize(width: 0, height: -60)//połowa wysokości koła
+                        self.Scale = 0.0//dąży do skali 0
+                    }
+                }
+                   
+        }
+        VStack {
+            Circle()
+                .foregroundColor(.blue)
+                .frame(width: 120, height: 120)
+            
+                .scaleEffect(Scale)
+                .offset(x: position2.width, y: position2.height)
+                .onAppear(){
+                    self.position2 = CGSize(width: 400, height: -300)
+                    withAnimation(.linear(duration: 2).repeatForever()) {
+                        self.position2 = CGSize(width: 0, height: -200)
+                        self.Scale = 0.0
+                    }
+                }
+                   
+        }
+        
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ na ostatnie laby/reszta/reszta/reszta/reszta.swift
@@ -0,0 +1,17 @@
+//
+//  animacja_lab4App.swift
+//  animacja_lab4
+//
+//  Created by apios on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct animacja_lab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
